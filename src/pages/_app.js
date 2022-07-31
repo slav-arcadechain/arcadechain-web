12464df37c891298e55b726fc2aa3e882d70be37
Slav <slav@arcadechain.io>
2022-07-31 12:23:08 +0100

    removing web3 libs adding html lang removing web3 libs adding html lang

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,8 @@
-import { Web3ReactProvider } from '@web3-react/core'
-import Web3 from 'web3'
 import '../../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  function getLibrary(provider) {
-    return new Web3(provider)
-  }
-
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
       <Component {...pageProps} />
-    </Web3ReactProvider>
   )
 }
 
